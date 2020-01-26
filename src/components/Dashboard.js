129d9f0f82dbf624f61df23c6c6f4377a97c5a6d
DHAKAL <aayushdhakal544@Gmail.com>
2020-01-27 04:50:55 +0545

    navbar

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,46 +2,48 @@ import React, { Component } from 'react'
 import { Container, Row, Col } from 'reactstrap'
 import { Navbar, UncontrolledDropdown,DropdownToggle, DropdownMenu, DropdownItem, } from 'reactstrap';
 import { Link, Redirect } from 'react-router-dom'
-import './Custom_css/login.css'
+import './Custom_css/dashboard.css'
 export default class Dashboard extends Component {
     render() {
         return (
           <div>
-            <Navbar id="navLogoText" className="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top">
-                <a class="navbar-brand"  href="#">
-                    {/* <img class="img-Logo" src={require('./CssImages/zippy_logo_trans.png')} alt="logoofzippy"/>  */}
-                    <a href="" className="dashtext">Zippy</a>
-                </a>
-               
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapseDIv" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="collapseDIv">
-                    {/* <!-- Left Side Of Navbar --> */}
-                    <div class="nav text-center collapse navbar-collapse font-weight-bold" id="collapseDIv">
-                        <ul class="navbar-nav" id="menuS">
-                        <li class="nav-item"><a href=""  class="nav-link icon home"> <p class="iconP">Home</p></a></li>
-                        </ul>
-                    </div>
-                </div>
+            <Navbar id="navLogoText" className="navbar navbar navbar-dark bg-dark navbar-expand-lg text-white navbar-fixed-top txt">
+                <Container>
+                  <a class="navbar-brand"  href="#">
+                      {/* <img class="img-Logo" src={require('./CssImages/zippy_logo_trans.png')} alt="logoofzippy"/>  */}
+                      <a href="" className="dashtext">Zippy</a>
+                  </a>
+                
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapseDIv" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                      <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="collapseDIv">
+                      {/* <!-- Left Side Of Navbar --> */}
+                      <div class="nav text-center collapse navbar-collapse font-weight-bold" id="collapseDIv">
+                          <ul class="navbar-nav" id="menuS">
+                          <li class="nav-item"><a href=""  class="nav-link icon home"> <p class="iconP">Home</p></a></li>
+                          </ul>
+                      </div>
+                  </div>
 
-                {/* <!-- Right Side Of Navbar --> */}
-                <ul class="navbar-nav ml-auto">
-                  <UncontrolledDropdown nav inNavbar style={{ marginRight: '20px' }}>
-                    <DropdownToggle nav caret>
-                      Username
-                    </DropdownToggle>
-                    <DropdownMenu right>
-                      <DropdownItem>
-                        Profile
-                      </DropdownItem>
-                      <DropdownItem divider />
-                      <DropdownItem>
-                        Logout
-                      </DropdownItem>
-                    </DropdownMenu>
-                  </UncontrolledDropdown>
-                </ul>
+                  {/* <!-- Right Side Of Navbar --> */}
+                  <ul class="navbar-nav ml-auto">
+                    <UncontrolledDropdown nav inNavbar style={{ marginRight: '20px' }}>
+                      <DropdownToggle nav caret>
+                        Username
+                      </DropdownToggle>
+                      <DropdownMenu right>
+                        <DropdownItem>
+                          Profile
+                        </DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem>
+                          Logout
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </UncontrolledDropdown>
+                  </ul>
+                </Container>
             </Navbar> 
 
           <h6 className="mb-3 mt-3 text-muted text-center"> Admin Dashboard </h6>
