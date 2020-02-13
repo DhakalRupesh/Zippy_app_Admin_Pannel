1989c8d179d3bed93f5c6522cde414744ae37bc9
DHAKAL <aayushdhakal544@Gmail.com>
2020-02-14 04:59:34 +0545

    vehicle

--- src/components/Vehicle.js
+++ src/components/Vehicle.js
@@ -4,14 +4,26 @@ import Nav from './SideTop'
 import './Custom_css/addadvertise.css'
 
 export default class Vehicle extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
     render() {
         return (
             <div>
                 <Nav></Nav>
                 <Sidebar></Sidebar>
-
                 <div className="add_user_Main">
-
+                    <h3 className="mb-4">Vehicle Info</h3>
+                    <div className="form-group">
+                        <label for="typesofdilevery">Type of dilevery</label>
+                        <input type="text" name="goodstype" className="form-control" placeholder="eg: room shift, docs, furniture" 
+                        value={this.state.goodstype} onChange={this.handleChange} required/>
+                    </div>
                 </div>
             </div>
         )
